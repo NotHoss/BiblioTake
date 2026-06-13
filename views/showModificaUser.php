282e0b5d6d81eddb4ba777346f042887052d6259
Tobia-Claut <tobiaclaut03@gmail.com>
2026-06-13 19:32:40 +0200

    fix(user)
    risolti conflitti merge e aggiunte piccole modifiche in style.css, showModificaUser.html e showModificaUser.php.
    Aggiunte immagini profilo

--- views/showModificaUser.php
+++ views/showModificaUser.php
@@ -8,8 +8,7 @@ if (empty($userInfo)) {
     $foto = !empty($userInfo['foto_profilo']) ? htmlspecialchars('/'.$userInfo['foto_profilo'], ENT_QUOTES, 'UTF-8') : '/images/default-avatar.jpg';
 
     $placeholders = [
-        '[TO-LOGIN]' => '',     //Rimuove il link di login poiché l'utente è già loggato
-        '[USERNAME]' => htmlspecialchars($userInfo['username'] ?? '', ENT_QUOTES, 'UTF-8'),
+        '[USERNAME]' => htmlspecialchars($user['username'] ?? '', ENT_QUOTES, 'UTF-8'),
         '[EMAIL]' => htmlspecialchars($userInfo['email'] ?? '', ENT_QUOTES, 'UTF-8'),
         '[PASSWORD]' => '********', //Voglio mostrare la password solo nella pagina di modifica delle informazioni 
         '[IMMAGINE_PROFILO]' => $foto,
